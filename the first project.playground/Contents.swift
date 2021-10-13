13484705d628e82b9d62705dad8cd705a5d41efb
lama <lamafarhan1997@gmail,com>
2021-10-13 12:05:00 +0300

    new edit

--- the first project.playground/Contents.swift
+++ the first project.playground/Contents.swift
@@ -1,6 +1,6 @@
-import UIKit
 import Foundation
-import Darwin
+
+
 
 enum Taskstatus {
     case completed
@@ -12,50 +12,73 @@ struct TaskItem {
     var title: String
     var description: String
     var priority: String
+    var taskstatus:Taskstatus
     
 }
 
-class ToDoList {
+struct List {
+    var title: String
+    var items: [TaskItem] = []
     
-    var tasks :[TaskItem]
-    init(){
-        tasks = []
-    }
-    
-    func addtask(todo:TaskItem) {
-        tasks.append(todo)
+    mutating func addtask(task:TaskItem) {
+        self.items.append(task)
         print("Addded Succefully")
     }
-       
-    func removeTask(index : Int) {
-        tasks.remove(at: index)
+    mutating func updateTaskFromList(index :Int , update: TaskItem ){
+        if (self.items.count > index){
+            self.items[index] = update
+            print("Task Updated Successfully")
+        } else {
+            print("Cannot find task")
+        }
+
+        
+    }
+    mutating func removeTaskFromList(index: Int){
+        if (self.items.count > index){
+        self.items.remove(at: index)
+        print("Task Deleted Successfully")
+        } else {
+            print("Item cannot be deleted")
+        }
+    }
+}
+class Todolist {
+    var list1 : [List]  = []
+    func removeTask(Listindex : Int, TaskItemindex : Int) {
         print("Deleted Succefully")
     }
     
-    func readTask(){
-        for list in tasks  {
-            print("\(list.title) ....  \(list.description)  ....  \(list.priority)")
+    func readTask() {
+        for item in list1 {
+            print(item.title)
+            for task in item.items {
+                print(task.title, task.description, task.priority)
+            }
         }
     }
     
-    func updateTask(index :Int , updat: TaskItem ){
-     tasks[index] = updat
-        print("Task Updated Successfully")
+
+    func addtask(add:List) {
+        self.list1.append(add)
+        print("added succefully")
     }
 }
 
-var myTask = ToDoList()
-var firsttask = TaskItem (title: "go to the university", description: "studying", priority: "completed")
-var secondtask = TaskItem  (title: "go to the jym", description: "exercises", priority: "pending")
-var thirdtask = TaskItem (title: "appointment", description: "check", priority: "completed")
+//             
 
-myTask.addtask(todo: firsttask)
-myTask.addtask(todo: secondtask)
-myTask.addtask(todo: thirdtask)
+var zerotask = TaskItem(title: "appointment", description: "check", priority: "high", taskstatus: .completed)
+var firsttask = TaskItem (title: "go to the university", description: "studying", priority: "med", taskstatus: .pending)
+var secondtask = TaskItem  (title: "go to the jym", description: "exercises", priority: "med", taskstatus: .pending)
+var thirdtask = TaskItem (title: "appointment", description: "check", priority: "high", taskstatus: .pending)
 
-myTask.updateTask(index: 2, updat:thirdtask)
+var myTask = List(title: "list1", items: [zerotask,firsttask])
+var ourtask = List(title: "list2", items:[secondtask,thirdtask])
 
-//myTask.removeTask(index: 0)
-//myTask.removeTask(index: 1)
+var myList = Todolist()
+myList.addtask(add: myTask)
+myList.addtask(add: ourtask)
+ 
 
-myTask.readTask()
+myList.list1[0].removeTaskFromList(index: 1)
+myList.readTask()
